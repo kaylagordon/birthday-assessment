4cfe07cc2b4fa90ea3aa82151258351632654aa3
Maria DelSignore <maria.ad27@gmail.com>
2021-09-26 09:21:47 -0600

    Builds out component contructor

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,16 @@
 import { Component } from 'react';
+import Form from '../Form/Form';
+import AllTheBirthdays from '../AllTheBirthdays/AllTheBirthdays';
 import months from '../../months_data';
 import './App.css';
 
 class App extends Component {
-
+  constructor() {
+    super();
+    this.state = {
+      bdayMonths: months
+    }
+  }
   
 
 
@@ -11,13 +18,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {console.log(months)}
+        {console.log(this.state.bdayMonths)}
         <h1>Birthdays</h1>
         <div className='bday-form'>
-
+          {/* <Form /> */}
         </div>
         <div className='bday-container'>
-
+          <AllTheBirthdays bdayMonths={this.state.bdayMonths}/>
         </div>
       </div>
     );
